fbfd51a60f96538c5696139f1c5d818be56a3ba8
Kelvin <ykekelvin0220@gmail.com>
2023-07-12 12:56:58 +0800

    reverse escape

--- functions/EscapePassword.java
+++ functions/EscapePassword.java
@@ -42,4 +42,39 @@ public class EscapePassword {
         }
         return sb.toString();
     }
+
+    public static String reverseEscapeHtml(String input) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < input.length()) {
+            char c = input.charAt(i);
+            if (c == '&') {
+                if (i + 4 < input.length() && input.substring(i, i + 5).equals("&amp;")) {
+                    sb.append('&');
+                    i += 5;
+                } else if (i + 3 < input.length() && input.substring(i, i + 4).equals("&lt;")) {
+                    sb.append('<');
+                    i += 4;
+                } else if (i + 3 < input.length() && input.substring(i, i + 4).equals("&gt;")) {
+                    sb.append('>');
+                    i += 4;
+                } else if (i + 5 < input.length() && input.substring(i, i + 6).equals("&quot;")) {
+                    sb.append('"');
+                    i += 6;
+                } else {
+                    sb.append(c);
+                    i++;
+                }
+            } else if (c == '\'') {
+                sb.append('\'');
+                i++;
+            } else {
+                sb.append(c);
+                i++;
+            }
+        }
+        return sb.toString();
+    }
 }
+
+
